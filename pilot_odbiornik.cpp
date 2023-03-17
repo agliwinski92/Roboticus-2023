7b2a178a7e6d16ef04371bcaf309ad88f729de4c
agliwinski92 <128039016+agliwinski92@users.noreply.github.com>
2023-03-17 12:25:44 +0100

    Update pilot_odbiornik.cpp

--- pilot_odbiornik.cpp
+++ pilot_odbiornik.cpp
@@ -3,6 +3,8 @@
 #define led_pin 13		// pin leda informacyjnego czy kod działa
 #define receive_pin 11 		// pin odbioru danych
 
+#define silnik_prawy 5		// pin sterowania silnikiem
+#define silnik_lewy 6 		// pin sterowania silnikiem
 
 void setup()
 {
@@ -18,19 +20,48 @@ void loop()
      // częśc wymagana do poprawnego działania biblioteki
     uint8_t buf[VW_MAX_MESSAGE_LEN];
     uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
-    if (vw_get_message(buf, &buflen)) // jeśli odbierzemy dane
+    
+    // jeśli odbierzemy dane
+    if (vw_get_message(buf, &buflen)) 
     {
 	int i;
-        String wiadomosc;
+	int status;
+        String odebrana_liczba;
 
-        digitalWrite(led_pin, HIGH); //zapalamy LED
+        digitalWrite(led_pin, HIGH); //zapalamy LED odbioru danych
         	
 	for (i = 0; i < buflen; i++) // w pętli zczytujemy dane z odbiornika
 	{
-            wiadomosc+=char(buf[i]);
+           odebrana_liczba+=char(buf[i]);
 	}
-	Serial.println(wiadomosc);
+	    
+	status = odebrana_liczba.toInt();    // zamieniamy odebrany znak tesktowy na liczbę
+	
+	switch (status) {
+		case 0:  // gdy status = 0 wykonaj:
+		{ 
+			digitalWrite(silnik_prawy, LOW); 
+			digitalWrite(silnik_lewy, LOW);
+			break;
+		}
+		case 1:  // gdy status = 1 wykonaj:
+		{ 
+			digitalWrite(silnik_prawy, LOW); 
+			digitalWrite(silnik_lewy, LOW);
+			break;
+		}
+		case 2:  // gdy status = 2 wykonaj:
+		{ 
+			digitalWrite(silnik_prawy, LOW); 
+			digitalWrite(silnik_lewy, LOW);
+			break;
+		}
+		default:
+		status = 0;
+        	break;
+		}
+  
+	
         digitalWrite(led_pin, LOW); // gasimy LED
     }
 }
@@ -38,25 +69,4 @@ void loop()
 
 
 
-
-
-    if (vw_get_message(buf, &buflen)) // jeśli odbierzemy dane
-    {
-	int i,pwm,wartosc;
-        String liczba;
-
-        digitalWrite(led_pin, HIGH); //zapalamy LED
-       // w pętli zczytujemy dane z odbiornika 
-        for (i = 0; i < buflen; i++) 
-        {
-          liczba+=char(buf[i]); // wiadomość
-        }
-        	
-        wartosc = liczba.toInt();
-        pwm = map(wartosc,0,1023,0,255);
-	Serial.print(wartosc);// Debug
-        Serial.print("   ");
-        Serial.println (pwm);
-        analogWrite(pwm_pin, pwm);
-        digitalWrite(led_pin, LOW); // gasimy LED
 
