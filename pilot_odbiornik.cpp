ef9b5b6459c75a113c224dd91536256b9bf0f237
agliwinski92 <128039016+agliwinski92@users.noreply.github.com>
2023-06-03 19:49:11 +0200

    Update pilot_odbiornik.cpp

--- pilot_odbiornik.cpp
+++ pilot_odbiornik.cpp
@@ -8,64 +8,40 @@
 
 void setup()
 {
+    Serial.begin(9600);
+    Serial.println("Setup ...");
 
-    vw_set_rx_pin(receive_pin);
+    vw_set_rx_pin(receive_pin); 		// przygotowujemy potrzebne informacje dla biblioteki
     vw_setup(2000);	 
-    vw_rx_start(); // startujemy odbieranie danych (uruchamiamy)
-    pinMode(led_pin, OUTPUT);
+    vw_rx_start(); 				// startujemy odbieranie danych (uruchamiamy)
+    
+    pinMode(silnik_prawy, OUTPUT);
+    pinMode(silnik_lewy, OUTPUT);
+    Serial.println("Setup zakończony.");
 }
 
 void loop()
 {
-     // częśc wymagana do poprawnego działania biblioteki
+  // częśc wymagana do poprawnego działania biblioteki
     uint8_t buf[VW_MAX_MESSAGE_LEN];
     uint8_t buflen = VW_MAX_MESSAGE_LEN;
-    
-    // jeśli odbierzemy dane
-    if (vw_get_message(buf, &buflen)) 
+ 
+    if (vw_get_message(buf, &buflen)) // jeśli odbierzemy dane
     {
-	int i;
-	int status;
-        String odebrana_liczba;
-
-        digitalWrite(led_pin, HIGH); //zapalamy LED odbioru danych
+	int i,wartosc;
+        String liczba;
+ 
+        digitalWrite(led_pin, HIGH);  //zapalamy LED
+       
+        for (i = 0; i < buflen; i++)  // w pętli zczytujemy dane z odbiornika 
+        {
+          liczba+=char(buf[i]); // wiadomość
+        }
         	
-	for (i = 0; i < buflen; i++) // w pętli zczytujemy dane z odbiornika
-	{
-           odebrana_liczba+=char(buf[i]);
-	}
-	    
-	status = odebrana_liczba.toInt();    // zamieniamy odebrany znak tesktowy na liczbę
-	
-	switch (status) {
-		case 0:  // gdy status = 0 wykonaj:
-		{ 
-			digitalWrite(silnik_prawy, LOW); 
-			digitalWrite(silnik_lewy, LOW);
-			break;
-		}
-		case 1:  // gdy status = 1 wykonaj:
-		{ 
-			digitalWrite(silnik_prawy, LOW); 
-			digitalWrite(silnik_lewy, LOW);
-			break;
-		}
-		case 2:  // gdy status = 2 wykonaj:
-		{ 
-			digitalWrite(silnik_prawy, LOW); 
-			digitalWrite(silnik_lewy, LOW);
-			break;
-		}
-		default:
-		status = 0;
-        	break;
-		}
-  
-	
-        digitalWrite(led_pin, LOW); // gasimy LED
+        wartosc = liczba.toInt();
+        Serial.print(wartosc);// Debug
     }
 }
-
 
 
 
